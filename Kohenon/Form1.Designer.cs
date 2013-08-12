5b44e53c974374381fd9c48a285ce136ff765cfb
Tushar Tiwari <tushart91@gmail.com>
2013-08-12 09:31:45 +0530

    Init Commit

---
+++ Kohenon/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Kohenon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.learningRate = new System.Windows.Forms.TextBox();
+            this.w00 = new System.Windows.Forms.TextBox();
+            this.w01 = new System.Windows.Forms.TextBox();
+            this.w10 = new System.Windows.Forms.TextBox();
+            this.w11 = new System.Windows.Forms.TextBox();
+            this.w20 = new System.Windows.Forms.TextBox();
+            this.w21 = new System.Windows.Forms.TextBox();
+            this.w30 = new System.Windows.Forms.TextBox();
+            this.w31 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.i0 = new System.Windows.Forms.TextBox();
+            this.i1 = new System.Windows.Forms.TextBox();
+            this.i2 = new System.Windows.Forms.TextBox();
+            this.i3 = new System.Windows.Forms.TextBox();
+            this.epochInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // learningRate
+            // 
+            resources.ApplyResources(this.learningRate, "learningRate");
+            this.learningRate.Name = "learningRate";
+            // 
+            // w00
+            // 
+            resources.ApplyResources(this.w00, "w00");
+            this.w00.Name = "w00";
+            // 
+            // w01
+            // 
+            resources.ApplyResources(this.w01, "w01");
+            this.w01.Name = "w01";
+            // 
+            // w10
+            // 
+            resources.ApplyResources(this.w10, "w10");
+            this.w10.Name = "w10";
+            // 
+            // w11
+            // 
+            resources.ApplyResources(this.w11, "w11");
+            this.w11.Name = "w11";
+            // 
+            // w20
+            // 
+            resources.ApplyResources(this.w20, "w20");
+            this.w20.Name = "w20";
+            // 
+            // w21
+            // 
+            resources.ApplyResources(this.w21, "w21");
+            this.w21.Name = "w21";
+            // 
+            // w30
+            // 
+            resources.ApplyResources(this.w30, "w30");
+            this.w30.Name = "w30";
+            // 
+            // w31
+            // 
+            resources.ApplyResources(this.w31, "w31");
+            this.w31.Name = "w31";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // i0
+            // 
+            resources.ApplyResources(this.i0, "i0");
+            this.i0.Name = "i0";
+            // 
+            // i1
+            // 
+            resources.ApplyResources(this.i1, "i1");
+            this.i1.Name = "i1";
+            // 
+            // i2
+            // 
+            resources.ApplyResources(this.i2, "i2");
+            this.i2.Name = "i2";
+            // 
+            // i3
+            // 
+            resources.ApplyResources(this.i3, "i3");
+            this.i3.Name = "i3";
+            // 
+            // epochInput
+            // 
+            resources.ApplyResources(this.epochInput, "epochInput");
+            this.epochInput.Name = "epochInput";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.epochInput);
+            this.Controls.Add(this.i3);
+            this.Controls.Add(this.i2);
+            this.Controls.Add(this.i1);
+            this.Controls.Add(this.i0);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.w31);
+            this.Controls.Add(this.w30);
+            this.Controls.Add(this.w21);
+            this.Controls.Add(this.w20);
+            this.Controls.Add(this.w11);
+            this.Controls.Add(this.w10);
+            this.Controls.Add(this.w01);
+            this.Controls.Add(this.w00);
+            this.Controls.Add(this.learningRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox learningRate;
+        private System.Windows.Forms.TextBox w00;
+        private System.Windows.Forms.TextBox w01;
+        private System.Windows.Forms.TextBox w10;
+        private System.Windows.Forms.TextBox w11;
+        private System.Windows.Forms.TextBox w20;
+        private System.Windows.Forms.TextBox w21;
+        private System.Windows.Forms.TextBox w30;
+        private System.Windows.Forms.TextBox w31;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox i0;
+        private System.Windows.Forms.TextBox i1;
+        private System.Windows.Forms.TextBox i2;
+        private System.Windows.Forms.TextBox i3;
+        private System.Windows.Forms.TextBox epochInput;
+    }
+}
+
---
+++ Kohenon/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kohenon
+{
+    public partial class Form1 : Form
+    {
+        double[,] weights = new double[4,2];
+        List<input> list = new List<input>();
+        int epoch;
+        double alpha;
+
+        input item;
+        int window = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            i0.Left = 127;
+            i0.Top = 29;
+            i1.Left = 147;
+            i1.Top = 29;
+            i2.Left = 167;
+            i2.Top = 29;
+            i3.Left = 187;
+            i3.Top = 29;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i0.Focus();
+            if (window == 1)
+            {
+                item = new input();
+                item.i[0] = int.Parse(i0.Text);
+                item.i[1] = int.Parse(i1.Text);
+                item.i[2] = int.Parse(i2.Text);
+                item.i[3] = int.Parse(i3.Text);
+                list.Add(item);
+                label3.Text = "Input " + list.Count.ToString() + " Added";
+                i0.Text = "";
+                i1.Text = "";
+                i2.Text = "";
+                i3.Text = "";
+            }
+            else
+            {
+                window = 1;
+                learningRate.Visible = false;
+                button2.Visible = true;
+                button3.Visible = true;
+                label1.Text = "Input Vectors";
+                button1.Text = "Add";
+
+                i0.Visible = true;
+                i1.Visible = true;
+                i2.Visible = true;
+                i3.Visible = true;
+
+                w00.ReadOnly = true;
+                w01.ReadOnly = true;
+                w10.ReadOnly = true;
+                w11.ReadOnly = true;
+                w20.ReadOnly = true;
+                w21.ReadOnly = true;
+                w30.ReadOnly = true;
+                w31.ReadOnly = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            window = 0;
+            learningRate.Visible = true;
+            button2.Visible = false;
+            button3.Visible = false;
+            button1.Text = "Next";
+            label1.Text = "Learning Rate";
+
+            i0.Visible = false;
+            i1.Visible = false;
+            i2.Visible = false;
+            i3.Visible = false;
+
+            w00.ReadOnly = false;
+            w01.ReadOnly = false;
+            w10.ReadOnly = false;
+            w11.ReadOnly = false;
+            w20.ReadOnly = false;
+            w21.ReadOnly = false;
+            w30.ReadOnly = false;
+            w31.ReadOnly = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (epochInput.Text == "")
+            {
+                i0.Enabled = i1.Enabled = i2.Enabled = i3.Enabled = false;
+                w00.Enabled = w01.Enabled = w10.Enabled = w11.Enabled = false;
+                w20.Enabled = w21.Enabled = w30.Enabled = w31.Enabled = false;
+                label3.Left = 80;
+                label3.Text = "Enter The no of Epochs";
+                epochInput.Visible = true;
+                epochInput.Focus();
+                button3.Text = "Compute";
+                button1.Enabled = button2.Enabled = false;
+                epochInput.Focus();
+            }
+            else if (epochInput.ReadOnly == false)
+            {
+                epochInput.ReadOnly = true;
+                button3.Text = "Exit";
+                extract();
+                compute();
+
+                w00.Text = weights[0, 0].ToString();
+                w01.Text = weights[0, 1].ToString();
+                w10.Text = weights[1, 0].ToString();
+                w11.Text = weights[1, 1].ToString();
+                w20.Text = weights[2, 0].ToString();
+                w21.Text = weights[2, 1].ToString();
+                w30.Text = weights[3, 0].ToString();
+                w31.Text = weights[3, 1].ToString();
+
+                label1.Text = "Final Weight Matrix";
+                label1.Left = 95;
+                label3.Text = "Finished";
+                label3.Left = 125;
+                epochInput.Visible = false;
+                i0.Visible = i1.Visible = i2.Visible = i3.Visible = false;
+
+                string message = "";
+                for (int j = 0; j < list.Count; j++)
+                {
+                    message += "Input " + (j + 1) + " Belongs to Cluster " + list[j].cluster + "\n\r";
+                }
+
+                MessageBox.Show(message, "Finished!!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+                Application.Exit();
+        }
+
+        private void extract()
+        {
+            weights[0, 0] = double.Parse(w00.Text);
+            weights[0, 1] = double.Parse(w01.Text);
+            weights[1, 0] = double.Parse(w10.Text);
+            weights[1, 1] = double.Parse(w11.Text);
+            weights[2, 0] = double.Parse(w20.Text);
+            weights[2, 1] = double.Parse(w21.Text);
+            weights[3, 0] = double.Parse(w30.Text);
+            weights[3, 1] = double.Parse(w31.Text);
+            epoch = int.Parse(epochInput.Text);
+            alpha = double.Parse(learningRate.Text);
+        }
+
+        private void compute()
+        {
+            double d1,d2;
+            while (epoch != 0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    d1 =  Math.Pow((list[i].i[0] - weights[0, 0]), 2) + Math.Pow((list[i].i[1] - weights[1, 0]), 2);
+                    d1 += Math.Pow((list[i].i[2] - weights[2, 0]), 2) + Math.Pow((list[i].i[3] - weights[3, 0]), 2);
+
+                    d2 =  Math.Pow((list[i].i[0] - weights[0, 1]), 2) + Math.Pow((list[i].i[1] - weights[1, 1]), 2);
+                    d2 += Math.Pow((list[i].i[2] - weights[2, 1]), 2) + Math.Pow((list[i].i[3] - weights[3, 1]), 2);
+
+                    if (d1 < d2 || d1.Equals(d2))
+                    {
+                        weights[0, 0] = weights[0, 0] + alpha * (list[i].i[0] - weights[0, 0]);
+                        weights[1, 0] = weights[1, 0] + alpha * (list[i].i[1] - weights[1, 0]);
+                        weights[2, 0] = weights[2, 0] + alpha * (list[i].i[2] - weights[2, 0]);
+                        weights[3, 0] = weights[3, 0] + alpha * (list[i].i[3] - weights[3, 0]);
+                        list[i].cluster = 1;
+                    }
+                    else
+                    {
+                        weights[0, 1] = weights[0, 1] + alpha * (list[i].i[0] - weights[0, 1]);
+                        weights[1, 1] = weights[1, 1] + alpha * (list[i].i[1] - weights[1, 1]);
+                        weights[2, 1] = weights[2, 1] + alpha * (list[i].i[2] - weights[2, 1]);
+                        weights[3, 1] = weights[3, 1] + alpha * (list[i].i[3] - weights[3, 1]);
+                        list[i].cluster = 2;
+                    }
+                }
+                epoch--;
+            }
+        }
+    }
+
+    class input
+    {
+        public int[] i;
+        public int cluster;
+        public input()
+        {
+            i = new int[4];
+        }
+    }
+}
